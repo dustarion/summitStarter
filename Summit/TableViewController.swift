ec3391ca1a516a73d30079d4591fdd1cf8c374a8
Dalton Ng <dustarion@gmail.com>
2019-12-27 22:45:28 +0800

    Works, but labels are screwed up!

--- Summit/TableViewController.swift
+++ Summit/TableViewController.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    // Darken Stackview
+}
+
 class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
@@ -24,6 +33,11 @@ class TableViewController: UITableViewController {
         return Mountains.count
     }
     
+    // Add this delegate to change cell height!
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         /// Version 1
         //let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
@@ -31,18 +45,26 @@ class TableViewController: UITableViewController {
         //return cell
         
         /// Version 2
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        let currentMountain = Mountains[indexPath.row]
-        cell.textLabel?.text = currentMountain.name
-        cell.detailTextLabel?.text = String(currentMountain.height) + " m"
+        //let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        //let currentMountain = Mountains[indexPath.row]
+        //cell.textLabel?.text = currentMountain.name
+        //cell.detailTextLabel?.text = String(currentMountain.height) + " m"
         //cell.imageView?.image = currentMountain.image
-        cell.imageView?.image = cropImageToSquare(currentMountain.image)
+        //cell.imageView?.image = cropImageToSquare(currentMountain.image)
         
         /// Version 3
         /// Improving the image in the cell
-        cell.imageView?.layer.borderWidth = 5
-        cell.imageView?.layer.borderColor = UIColor.white.cgColor
-        cell.imageView?.clipsToBounds = true
+        //cell.imageView?.layer.borderWidth = 5
+        //cell.imageView?.layer.borderColor = UIColor.white.cgColor
+        //cell.imageView?.clipsToBounds = true
+        
+        /// Version 4
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nicerCell", for: indexPath) as! CustomTableViewCell
+        let currentMountain = Mountains[indexPath.row]
+        
+        cell.backgroundImageView.image = currentMountain.image
+        cell.titleLabel.text = currentMountain.name
+        cell.subtitleLabel.text = String(currentMountain.height) + " m"
         
         return cell
     }
