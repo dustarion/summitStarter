703da3872c05af0cceafe134ead9758fd721f6cd
Dalton Ng <dustarion@gmail.com>
2019-12-27 22:37:49 +0800

    Added custom cell storyboard code

---
+++ Summit/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  Summit
+//
+//  Created by Dalton Prescott Ng on 27/12/19.
+//  Copyright © 2019 daltonDemo. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+}
+
+
